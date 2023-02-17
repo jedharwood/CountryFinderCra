6489bc41163faad473bece8f25f5796db8e225b1
Jed Harwood <scaryteeth@hotmail.com>
2023-02-17 16:12:28 +0900

    test commit

--- src/js/pages/home.page.tsx
+++ src/js/pages/home.page.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 
 export const HomePage: React.FunctionComponent = () => {
     // return type?
-    return <h1>This is the home page</h1>;
+    return <h1>This is the home page Test commit</h1>;
 }
