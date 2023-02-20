1a591465ebae2bbc35937f54f6dabd6df1bdb5cf
Jed Harwood <scaryteeth@hotmail.com>
2023-02-20 10:56:49 +0900

    Styled header

--- src/js/components/header.tsx
+++ src/js/components/header.tsx
@@ -3,8 +3,17 @@ import { Link } from 'react-router-dom';
 
 export const Header: React.FunctionComponent = () => {
   return (
-    <nav>
-      <Link to="/">Where in the world</Link>
-    </nav>
+    <header className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3 shadow-md">
+      <nav>
+        <div className="px-4 mx-auto flex">
+          <Link
+            to="/"
+            className="text-xl font-bold text-slate-800 whitespace-nowrap cursor:pointer hover:underline"
+          >
+            Where in the world?
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 };
