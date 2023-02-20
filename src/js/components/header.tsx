be64e662e9c585ea49df8f02564fece5568e5b45
Jed Harwood <scaryteeth@hotmail.com>
2023-02-20 11:10:05 +0900

    basic search bar

--- src/js/components/header.tsx
+++ src/js/components/header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const Header: React.FunctionComponent = () => {
   return (
-    <header className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3 shadow-md">
+    <header className="relative flex flex-wrap items-center justify-between px-2 py-3 shadow-md">
       <nav>
         <div className="px-4 mx-auto flex">
           <Link
