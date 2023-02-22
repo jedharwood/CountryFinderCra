896f44d1e507a29f948f774a9bd9c5c34ae7f70e
Jed Harwood <scaryteeth@hotmail.com>
2023-02-22 10:40:41 +0900

    Added clear options to search inputs and refactored searchbar code

--- src/js/components/select-input.tsx
+++ src/js/components/select-input.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import Select from 'react-select';
 import { AppContext } from '../context/app-context';
 import { type SelectOption } from '../types/types';
+import { RESET_OPTIONS } from './search-bar';
 
 interface SelectProps {
   isSearchable: boolean;
@@ -15,13 +16,17 @@ export const SelectInput: React.FunctionComponent<SelectProps> = (
 ) => {
   const { loading } = useContext(AppContext);
   const { isSearchable, placeHolder, options, onChange } = props;
+  const clearOption: SelectOption = {
+    label: 'Clear options',
+    value: RESET_OPTIONS,
+  };
 
   return (
     <div className="w-full shadow-md">
       <Select
         required={true}
         isSearchable={isSearchable}
-        options={options}
+        options={[clearOption, ...options]}
         onChange={onChange}
         placeholder={placeHolder}
         isLoading={loading}
